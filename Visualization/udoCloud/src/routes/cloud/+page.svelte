af5f46215f65a6708c3210abdc46a773b1819c8f
Ynfiniti <sean_schwarz@gmx.de>
2023-11-25 16:21:43 +0100

    use store for cloud

--- Visualization/udoCloud/src/routes/cloud/+page.svelte
+++ Visualization/udoCloud/src/routes/cloud/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import {page} from "$app/stores";
-  import type {CloudSearchInputs} from "$lib/SearchInput/cloud/cloudSearchInputTypes";
+  import { page } from "$app/stores";
+  import type { CloudSearchInputs } from "$lib/SearchInput/cloud/cloudSearchInputTypes";
   import CloudSearchInput from "$lib/SearchInput/cloud/CloudSearchInput.svelte";
-
+  import { dbStore } from "$lib/database/dbStore";
 
     let initialValues= {
       initialDateMin: $page.url.searchParams.get('dateMin') || "1852-01",
@@ -10,10 +10,21 @@
       initialForTopic: $page.url.searchParams.get('forTopic') == "false",
     }
 
+    $: console.log($dbStore)
+
     function searchInputSubmit(e: CustomEvent<CloudSearchInputs>){
+      dbStore.loadNewData(e.detail, false)
       console.log("Submit", e.detail)
     }
 
 </script>
 
 <CloudSearchInput {...initialValues} on:submit={searchInputSubmit}></CloudSearchInput>
+
+<ul>
+    {#each $dbStore["cloud"] as {name, amount} }
+        <li>
+            <p>{name} ({amount})</p>
+        </li>
+    {/each}
+</ul>
