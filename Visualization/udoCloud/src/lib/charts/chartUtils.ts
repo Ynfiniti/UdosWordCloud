960482c45c21bbea514dbc67f3aea5bdd657fbc2
Ynfiniti <sean_schwarz@gmx.de>
2023-12-10 13:37:04 +0100

    remove file initialisation

--- Visualization/udoCloud/src/lib/charts/chartUtils.ts
+++ Visualization/udoCloud/src/lib/charts/chartUtils.ts
@@ -1,7 +1,7 @@
 import {writable} from "svelte/store";
 
 function createLoadingStore() {
-    const {subscribe, set} = writable(false)
+    const {subscribe, set} = writable(true)
 
     return {
         subscribe,
--- Visualization/udoCloud/src/routes/+layout.svelte
+++ Visualization/udoCloud/src/routes/+layout.svelte
@@ -1,19 +1,5 @@
 <script lang="ts">
     import '@carbon/charts-svelte/styles.css'
-    import {onMount} from 'svelte';
-    import {loadingStore} from "$lib/charts/chartUtils";
-
-    onMount(async () => {
-        loadingStore.set(true)
-        fetch("api/data").then((res) => {
-            res.json().then(j => {
-                console.log("Files loaded: ", j)
-                loadingStore.set(false)
-            })
-        })
-    });
-
-
 </script>
 
 <h1>Welcome to UdoCloud</h1>
