f68245484904a3dd8b00a6e746b44365d46ef226
Ynfiniti <sean_schwarz@gmx.de>
2023-12-13 19:34:50 +0100

    include udo image

--- Visualization/udoCloud/src/routes/+layout.svelte
+++ Visualization/udoCloud/src/routes/+layout.svelte
@@ -2,7 +2,23 @@
     import '@carbon/charts-svelte/styles.css'
 </script>
 
-<h1>Welcome to UdoCloud</h1>
+<style>
+	#header{
+			display: flex;
+      align-items: center;
+	}
+
+	img{
+			height: 2em;
+	}
+</style>
+
+	<h1>
+		<div id="header">
+			<img src="/Udo.png" alt="Udo"/>
+			Welcome to UdoCloud
+		</div>
+	</h1>
 <a href="/">Home</a>
 <br>
 <slot/>
